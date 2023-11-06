f5d1fb22e15e84c20e3cb18587d87ecd48af95c1
sadiaahmed114 <sadiaahmed26373@gmail.com>
2023-11-06 08:30:42 -0800

    all projects done

---
+++ Project 09/countdown timer/index.js
@@ -0,0 +1,26 @@
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please enter the amount of second",
+});
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    setInterval((() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log('Timer has expired');
+        }
+        const min =Math.floor((timeDiff %(3600+24))/3600);
+        const sec =Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2,"0")}:${sec.toString().padStart(2,"0")}`);
+    }),1000);
+}
+
+startTime(input);
+
+
+
